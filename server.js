1822edb922351361b4039f2bf193c046b9b713f4
jstnpdrs <pedrosaj24@example.com>
2022-09-26 15:43:30 +0800

    cors update

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(cors({
     origin: '*',
     'Access-Control-Allow-Origin': '*',
 }));
+app.options('*', cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
