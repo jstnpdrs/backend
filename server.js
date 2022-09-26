5006871c47f7075ac4036d9b28909d5e18194f23
jstnpdrs <pedrosaj24@example.com>
2022-09-26 13:56:34 +0800

    added cors header

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "acc-attendance.onrender.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/courses', require('./routes/courseRoutes'))
